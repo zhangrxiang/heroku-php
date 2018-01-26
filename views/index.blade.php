8460572142e5defb7d46346a2bc6ac4af284242d
zhangrxiang <599490911@qq.com>
2018-01-26 16:33:46 +0800

    github-api blade

---
+++ views/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<style>
+    * {
+        font-family: "Segoe Script", serif;
+    }
+
+    h1 {
+        text-align: center;
+    }
+    h3{
+        text-align: right;
+        margin-right: 30%;
+    }
+</style>
+<body>
+<h1>Hello World</h1>
+<h3>PHP version  {{ $version }}</h3>
+</body>
+</html>
